b0b762456e6d74926fcbc4247f3cd9b83eb07e84
Gayatri Aurangabadkar <aurangabadkargayatri@gmail.com>
2022-10-28 13:32:02 +0530

    modified first

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
-print("hello")
-print("Kya bolti public")
+print("How are you?")
+a= int(input("Enter a 1 or 2 \n"))
+if a==1:
+    print("Good")
+elif a==2:
+    print(" Not good ")
+else :
+    print("Zindagi barbaad hai")
